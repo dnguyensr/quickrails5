746559b19db56dbb1fc8fe1eb199e6d393facf9c
Daniel <danielnguyensr@gmail.com>
2018-01-22 01:04:27 -0800

    Add pages controller, views, routes; fix binstubs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  PagesController.action_methods.each do |action|
+    get "/#{action}", to: "pages##{action}", as: "#{action}_page"
+  end
 end
